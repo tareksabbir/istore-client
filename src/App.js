d13ea3a37c608e966d7ecc920e207d273efe40ce
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-05-07 23:34:21 +0600

    about us and footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Footer from './Pages/Footer/Footer';
 import Header from './Pages/Header/Header';
 import Navbar from './Pages/Navbar/Navbar';
 
@@ -9,7 +11,9 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Header></Header>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
       </Routes>
+      <Footer></Footer>
 
     </div>
   );
