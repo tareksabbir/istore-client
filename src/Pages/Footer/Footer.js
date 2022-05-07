ec93796353defe2ddf746556f2177f1c23679c62
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-05-08 00:53:01 +0600

    newsletter update

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -3,7 +3,26 @@ import logo from '../../Assets/images (1).png'
 
 const Footer = () => {
     return (
-        <>
+        <>  <div class="bg-white">
+            <div class="max-w-screen-2xl mx-auto">
+                <div class="flex flex-col items-center bg-gray-100 rounded-lg p-4 sm:p-8">
+                    <div class="mb-4 sm:mb-8">
+                        <h2 class="text-gray-600 text-xl sm:text-2xl lg:text-3xl font-bold text-center">Get the latest updates</h2>
+                        <p class="text-gray-500 text-center">Sign up for our newsletter</p>
+                    </div>
+
+                    <form class="w-full max-w-md flex gap-2 mb-3 sm:mb-5">
+                        <input placeholder="Email" class="w-full flex-1 bg-gray-white text-gray-800 placeholder-gray-400 border border-gray-300 focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+
+                        <button class="inline-block bg-gray-800 hover:bg-gray-500  focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded outline-none transition duration-100 px-8 py-2">Send</button>
+                    </form>
+
+                    <p class="text-gray-400 text-xs text-center">By signing up to our newsletter you agree to our <a href="/" class="hover:text-indigo-500 active:text-indigo-600 underline transition duration-100">Term of Service</a> and <a href="/" class="hover:text-indigo-500 active:text-indigo-600 underline transition duration-100">Privacy Policy</a>.</p>
+                </div>
+            </div>
+        </div>
+
+
             <footer class="bg-black">
 
                 <div class="pt-12 lg:pt-16">
