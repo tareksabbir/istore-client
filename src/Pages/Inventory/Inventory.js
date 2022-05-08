b585a8b436ea06b3c3ca89f122b1983c85ff3ea8
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-05-08 17:35:38 +0600

    inventory and manage inventory  data update from db

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -1,49 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Inventory = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/product')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, [])
+    const displayContent = (text) => {
+        return text.length < 60 ? text : text.slice(0, 60) + "...";
+    };
+
     return (
         <>
+
             <section>
                 <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-8 xl:mb-12 mt-5">Inventory Items</h2>
+                <p class="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto">In official product you get the guarantee to get inactive/intact product along with original accessories. On the other hand, in unofficial product we have observed replacement of original accessories with lookalike copy, aside in more than 60% cases customer gets active product.</p>
                 <div class="relative px-4 py-12 sm:px-6 lg:py-16 lg:px-8">
-                    <div class="relative mx-auto max-w-7xl">
-                        <div class="grid max-w-lg gap-5 mx-auto lg:grid-cols-3 lg:max-w-none">
-                            <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
-                                <div class="flex-shrink-0">
-                                    <img class="object-cover w-full h-48" src="https://images.unsplash.com/photo-1510878933023-e2e2e3942fb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80" alt="" />
-                                </div>
-                                <div class="flex flex-col justify-between flex-1 p-6 bg-white">
-                                    <div class="flex-1">
+                    <div class="relative mx-auto max-w-7xl lg:px-10">
+                        <div class="grid max-w-lg gap-5 lg:gap-16 mx-auto lg:grid-cols-3 lg:max-w-none">
 
-                                        <p class=" block mt-2 text-xl font-semibold text-neutral-600">Iphone X</p>
-                                        <p class="mt-3 text-base text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            {
+                                products.slice(0, 6).map(product => <div
+                                    key={product._id}>
 
-                                        <p class="text-lg font-semibold text-neutral-600 mt-5">Available Product 100</p>
-                                        <div className="flex items-end gap-1 mt-3">
-                                            <span className="self-start text-gray-600">$</span>
-                                            <span className="text-2xl text-gray-800 font-bold">1000</span>
-                                            <span className="text-gray-500">per/product</span>
+                                    <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
+                                        <div class="flex-shrink-0">
+                                            <img class="object-cover w-full h-48" src={product.img} alt="" />
                                         </div>
+                                        <div class="flex flex-col justify-between flex-1 p-6 bg-white">
+                                            <div class="flex-1">
 
+                                                <p class=" block mt-2 text-xl font-semibold text-neutral-600">{product.name}</p>
+                                                <p class="mt-3 text-base text-gray-500">{displayContent(product.description)}</p>
 
-                                    </div>
-                                    <div class="flex justify-between">
-                                        <div class="flex items-center mt-6">
-                                            <div>
-                                                <p class="text-sm font-medium text-neutral-600">
-                                                    Michaerl Andreuzza
-                                                </p>
+                                                <p class="text-lg font-semibold text-neutral-600 mt-5">Available Product {product.stock}</p>
+                                                <div className="flex items-end gap-1 mt-3">
+                                                    <span className="self-start text-gray-600">$</span>
+                                                    <span className="text-2xl text-gray-800 font-bold">{product.price}</span>
+                                                    <span className="text-gray-500">per/product</span>
+                                                </div>
+
 
                                             </div>
+                                            <div class="flex justify-between">
+                                                <div class="flex items-center mt-6">
+                                                    <div>
+                                                        <p class="text-sm font-medium text-neutral-600">
+                                                            Supplier : {product.supplier}
+                                                        </p>
 
-                                        </div>
-                                        <div className='mt-6'>
-                                            <button class="inline-block bg-gray-800 hover:bg-gray-500  focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded outline-none transition duration-100 px-8 py-2">Update</button>
-                                        </div>
+                                                    </div>
 
+                                                </div>
+                                                <div className='mt-6'>
+                                                    <button class="inline-block bg-gray-800 hover:bg-gray-500  focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded outline-none transition duration-100 px-8 py-2">Update</button>
+                                                </div>
+
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
+
+
+                                </div>)
+
+                            }
+
+
+
+
                         </div>
                     </div>
                 </div>
--- src/Pages/Inventory/ManageInventory.js
+++ src/Pages/Inventory/ManageInventory.js
@@ -1,69 +1,102 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ManageInventory = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/product')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, [])
+
     return (
-        <div class="bg-white py-6 sm:py-8 lg:py-12">
-            <div class="max-w-screen-lg px-4 md:px-8 mx-auto">
-                <div class="mb-6 sm:mb-10 lg:mb-16">
-                    <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Manage Inventory</h2>
-                </div>
+        <>
+            <div class="mb-6 sm:mb-10 lg:mb-10">
+                <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Manage Inventory</h2>
+            </div>
 
-                <div class="flex flex-col sm:border-t sm:border-b sm:divide-y mb-5 sm:mb-8">
 
-                    <div class="py-5 sm:py-8">
-                        <div class="flex flex-wrap gap-4 lg:gap-6 sm:py-2.5">
-                            <div class="sm:-my-2.5">
-                                <a href="/" class="group w-24 sm:w-40 h-40 sm:h-56 block bg-gray-100 rounded-lg overflow-hidden relative">
-                                    <img src="https://images.unsplash.com/photo-1620049185596-1f16f414c448?auto=format&q=75&fit=crop&w=200" loading="lazy" alt="" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
-                                </a>
-                            </div>
+            {
+                products.map(product => <div
+                    key={product._id}>
+                    <div class="bg-white py-6 sm:py-8 lg:py-2">
+                        <div class="max-w-screen-lg px-4 md:px-8 mx-auto">
 
-                            <div class="flex flex-col justify-between flex-1">
-                                <div>
-                                    <a href="/" class="inline-block text-gray-800 hover:text-gray-500 text-lg lg:text-xl font-bold transition duration-100 mb-1">Iphone 13</a>
 
-                                    <span class="block text-gray-500">Description: 128GB 4GB RAM Super Retina XDR OLED, HDR10, Dolby Vision, 800 nits (HBM), 1200 nits </span>
-                                    <span class="block text-gray-500">Color: White</span>
-                                </div>
+                            <div class="flex flex-col sm:border-t sm:border-b sm:divide-y mb-5 sm:mb-8">
 
-                                <div>
-                                    <span class="block text-gray-800 md:text-lg font-bold mb-1">$1000 <span><small> /per product</small></span></span>
+                                <div class="py-5 sm:py-8">
+                                    <div class="flex flex-wrap gap-4 lg:gap-6 sm:py-2.5">
+                                        <div class="sm:-my-2.5">
+                                            <a href="/" class="group w-24 sm:w-40 h-40 sm:h-56 block bg-gray-100 rounded-lg overflow-hidden relative">
+                                                <img src={product.img} loading="lazy" alt="" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
+                                            </a>
+                                        </div>
 
-                                    <span class="flex items-center text-gray-500 text-sm gap-1">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-                                        </svg>
+                                        <div class="flex flex-col justify-between flex-1">
+                                            <div>
+                                                <a href="/" class="inline-block text-gray-800 hover:text-gray-500 text-lg lg:text-2xl font-bold transition duration-100 mb-1">{product.name}</a>
+                                                <span class="mb-2 flex items-center text-gray-500 text-sm gap-1">
 
-                                        In stock
-                                    </span>
-                                </div>
-                            </div>
+                                                    Supplier : {product.supplier}
+                                                </span>
 
-                            <div class="w-full sm:w-auto flex justify-between border-t sm:border-none pt-4 sm:pt-0">
-                                <div class="flex flex-col items-start gap-2">
-                                    <div class="w-20 h-12 flex border rounded overflow-hidden">
-                                        <input type="number" value="1" class="w-full focus:ring ring-inset ring-indigo-300 outline-none transition duration-100 px-4 py-2" />
+                                                <span class="block text-gray-500"></span>
+                                                <span class="block text-gray-500 mb-2"><b>Description :</b> {product.description}</span>
+                                            </div>
 
-                                        <div class="flex flex-col border-l divide-y">
-                                            <button class="w-6 flex justify-center items-center flex-1 bg-white hover:bg-gray-100 active:bg-gray-200 leading-none select-none transition duration-100">+</button>
-                                            <button class="w-6 flex justify-center items-center flex-1 bg-white hover:bg-gray-100 active:bg-gray-200 leading-none select-none transition duration-100">-</button>
+                                            <div>
+                                                <span class="block text-gray-800 md:text-lg font-bold mb-1">$ <span>{product.price}</span> <span><small> /per product</small></span></span>
+
+                                                <span class="flex items-center text-gray-500 text-sm gap-1">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                                                    </svg>
+
+                                                    In stock
+                                                </span>
+
+                                            </div>
+                                        </div>
+
+                                        <div class="w-full sm:w-auto border-t sm:border-none pt-4 sm:pt-0 lg:mt-8">
+                                            <div class="pt-3 sm:pt-2 ml-4 md:ml-8 lg:ml-10 lg:mr-8 lg:mb-5">
+                                                <span class="block text-gray-800 md:text-lg font-bold">Total stock {product.stock}</span>
+                                            </div>
+                                            <form action="">
+                                                <input name="reset" className=" w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-2 py-2 mb-5" required type="text" placeholder='Reset stock' />
+                                            </form>
+
+                                            <div>
+                                                <button class="text-white hover:text-indigo-600 active:text-indigo-700 text-sm font-semibold transition duration-100 bg-black px-2 py-1  rounded lg:ml-16 ">Delete</button>
+                                            </div>
+                                            <div className='lg:mt-5'>
+                                                <button class="text-white hover:text-black active:text-indigo-700 text-sm font-semibold transition duration-100 bg-gray-500 px-2 py-1  rounded lg:ml-16 ">Update</button>
+                                            </div>
+
+
                                         </div>
                                     </div>
-
-                                    <button class="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 text-sm font-semibold select-none transition duration-100">Delete</button>
                                 </div>
 
-                                <div class="pt-3 sm:pt-2 ml-4 md:ml-8 lg:ml-16">
-                                    <span class="block text-gray-800 md:text-lg font-bold">total stock 150</span>
-                                </div>
                             </div>
+
                         </div>
                     </div>
 
-                </div>
+                </div>)
+            }
 
-            </div>
-        </div>
+
+
+
+
+
+
+
+
+
+        </>
 
     );
 };
